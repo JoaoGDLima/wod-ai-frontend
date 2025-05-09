2e828e49f1a242681f089ba125284217c4e174eb
JoaoGDLima <joaolima@cta.com.br>
2025-05-09 13:19:03 -0300

    [REFACOR]: Corrige erro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
+export default ({ mode }: any) => {
   const env = loadEnv(mode, process.cwd())
 
   return defineConfig({
